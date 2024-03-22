6e7b0a8ae1a36eab94645ada586ddeeda6539386
Noah Frank <nolefr@posteo.de>
2024-03-22 15:06:12 +0100

    Update user schema

--- models/User.js
+++ models/User.js
@@ -7,22 +7,27 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
     password: {
         type: String,
         required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
     }
-});
+
+}, { timestamps: true });
+
 
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
+
     this.password = await bcrypt.hash(this.password, 12);
     next();
 });
+
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export default model('User', userSchema);
