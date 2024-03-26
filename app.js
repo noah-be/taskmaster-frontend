d731b0fe759b7d79d09c58a28cbde3fccd65840e
Noah Frank <nolefr@posteo.de>
2024-03-26 02:14:59 +0100

    Improve logging style

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import routes from './routes/index.js';
+import handle404 from './middlewares/handle404.js';
 import('dotenv').then((dotenv) => dotenv.config());
 
 
@@ -10,11 +12,23 @@ const app = express();
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp';
 
 mongoose.connect(mongoURI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB:', err))
+    .then(() => {
+        const currentTime = new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        });
+        console.log(`[${currentTime}] \x1b[92m\x1b[1m[MongoDB]\x1b[0m ✅`);
+    })
+    .catch(err => console.error('Could not connect to MongoDB:', err));
 
 app.set('view engine', 'ejs');
 app.set('etag', 'strong');
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,10 +36,6 @@ app.use(express.static("public"));
 app.use('/api/task', routes.taskRoutes);
 app.use('/api/auth', routes.authRoutes);
 app.use('/', routes.generalRoutes);
-
-
-app.use((req, res) => {
-    res.status(404).render('main', { content: '404' });
-});
+app.use(handle404);
 
 export default app;
--- server.js
+++ server.js
@@ -6,7 +6,15 @@ let server;
 
 export function startServer() {
     server = app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+        const serverDomain = process.env.SERVER_DOMAIN || 'localhost';
+
+        const currentTime = new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        });
+
+        console.log(`[${currentTime}] \x1b[94m\x1b[1m[Server]\x1b[0m \x1b[94mhttp://${serverDomain}:${port} 🚀\x1b[0m`);
     });
     return server;
 }
