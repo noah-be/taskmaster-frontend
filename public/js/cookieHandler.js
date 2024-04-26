ebe2dad2d1a4fbb8fffa3c1315c244282ae6b2e5
Noah Frank <nolefr@posteo.de>
2024-04-26 13:52:39 +0200

    Refactor: Use let instead of var

--- public/js/cookieHandler.js
+++ public/js/cookieHandler.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cookieConsentBanner = document.getElementById("cookie-consent-banner");
-    var acceptCookieConsent = document.getElementById("accept-cookie-consent");
+document.addEventListener("DOMContentLoaded", function () {
+  let cookieConsentBanner = document.getElementById("cookie-consent-banner");
+  let acceptCookieConsent = document.getElementById("accept-cookie-consent");
 
-    if (!getCookie("cookie_consent")) {
-        cookieConsentBanner.style.display = "block";
+  if (!getCookie("cookie_consent")) {
+    cookieConsentBanner.style.display = "block";
+  }
+
+  acceptCookieConsent.onclick = function () {
+    setCookie("cookie-consent", "accepted", 7);
+    cookieConsentBanner.style.display = "none";
+  };
+
+  function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
     }
+    let cookieString =
+      name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
+    document.cookie = cookieString;
+  }
 
-    acceptCookieConsent.onclick = function() {
-        setCookie("cookie-consent", "accepted", 7);
-        cookieConsentBanner.style.display = "none";
-    };
-
-    function setCookie(name, value, days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        var cookieString = name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
-        document.cookie = cookieString;
+  function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
-
-    function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-
+    return null;
+  }
 });
