6fee21cf3e86fb054d787d9197292e8987b98a95
noah-frank <nolefr@posteo.de>
2024-11-18 03:40:46 +0100

    Update views

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="home">
+      <img
+        id="todo-list-img"
+        src="/images/todo_list.jpg"
+        class="rounded-image"
+        alt="A table on which there are plants, paper clips, a clipboard, an alarm clock and a ballpoint pen."
+      />
+    </div>
+  </main>
 </template>
