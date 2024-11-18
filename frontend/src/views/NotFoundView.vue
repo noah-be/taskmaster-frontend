6b63116e83860b22f4028a8c95b5b5b88122f115
noah-frank <nolefr@posteo.de>
2024-11-18 02:51:55 +0100

    Set up basic structure

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <main>
+    <img
+      id="cat-404"
+      src="/images/404_cat.jpeg"
+      alt="A sad looking cat and the white text 404 not found."
+    />
+    <button href="/" id="home-btn">Bring Me Home</button>
+
+    <!-- TODO: Add button function -->
+  </main>
+</template>
