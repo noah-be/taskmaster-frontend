01c7d0891081a671b39d3a2a79a96383c49aa66d
Noah <10984564+noah-frank@users.noreply.github.com>
2024-11-21 10:44:35 +0100

    Add login form

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form id="login-form" @submit.prevent="submitLogin">
+        <h2>Login</h2>
+        <label for="login-username">Username</label>
+        <input type="text" id="login-username" v-model="username" placeholder="Username" autocomplete="username" />
+
+        <label for="login-password">Password</label>
+        <input type="password" id="login-password" v-model="password" placeholder="Password"
+            autocomplete="current-password" />
+
+        <button id="login-btn" type="submit">Log In</button>
+        <hr />
+        <button id="create-new-account-btn" class="create-new-account-btn" @click.prevent="createNewAccount">
+            Create new account
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        submitLogin() {
+            // TODO: Handle login
+        },
+        createNewAccount() {
+            // TODO: Handle  create new account
+        },
+    },
+};
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,14 @@
-<script setup></script>
+<script setup>
+import LoginForm from "@/components/LoginForm.vue";</script>
 
 <template>
   <main>
     <div class="home">
-      <img
-        id="todo-list-img"
-        src="/images/todo_list.jpg"
-        class="rounded-image"
-        alt="A table on which there are plants, paper clips, a clipboard, an alarm clock and a ballpoint pen."
-      />
+
+      <LoginForm />
+
+      <img id="todo-list-img" src="/images/todo_list.jpg" class="rounded-image"
+        alt="A table on which there are plants, paper clips, a clipboard, an alarm clock and a ballpoint pen." />
     </div>
   </main>
 </template>
