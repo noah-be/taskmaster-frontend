ad7074411227697b8bc584824b3b567673a5618e
noah-frank <nolefr@posteo.de>
2024-11-22 08:03:32 +0100

    Delete old files

--- old_frontend/test/frontend.test.js
+++
@@ -1,7 +0,0 @@
-import { runFunctionalTests } from "./functionalTests.js";
-import { runAccessibilityTests } from "./accessibilityTests.js";
-
-describe("Frontend Tests", () => {
-  runFunctionalTests();
-  runAccessibilityTests();
-}, 15000);
--- old_frontend/test/jestConsoleMock.js
+++
@@ -1,9 +0,0 @@
-if (process.env.STYLE == "clean") {
-  global.console = {
-    ...console,
-    log: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn(),
-    debug: jest.fn(),
-  };
-}
