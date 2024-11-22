8a139298b5bf3c67de8db78b473e887c47258f2b
noah-frank <nolefr@posteo.de>
2024-11-22 10:13:53 +0100

    Delete obsolete routes and middleswares

--- backend/src/middlewares/notFoundMiddleware.js
+++
@@ -1,7 +0,0 @@
-const handle404 = (req, res, next) => {
-    res.status(404).render('main-layout', {
-        content: '404'
-    });
-};
-
-export default handle404;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,4 @@
 import taskRoutes from "./taskRoutes.js";
 import authRoutes from "./authRoutes.js";
-import generalRoutes from "./generalRoutes.js";
 
-export default { authRoutes, taskRoutes, generalRoutes };
+export default { authRoutes, taskRoutes };
