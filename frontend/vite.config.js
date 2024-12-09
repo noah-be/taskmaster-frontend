c0f472d4c0eaca2accd431c5addcaa29fc292242
Noah <10984564+noah-be@users.noreply.github.com>
2024-12-09 03:12:12 +0100

    Revert "Fix test setup and update snapshots"
    This reverts commit 8e9119de17f9b1e2b23777b3176bb64320f8d240.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vuetify({ autoImport: true }), vueDevTools()],
   test: {
     globals: true,
     environment: "jsdom",
