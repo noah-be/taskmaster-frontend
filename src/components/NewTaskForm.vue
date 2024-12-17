08a1ee141e051835e9c802d3b5e24797078c5068
noah-frank <nolefr@posteo.de>
2024-12-17 18:48:18 +0100

    Update NewTaskForm component to use localization

--- src/components/NewTaskForm.vue
+++ src/components/NewTaskForm.vue
@@ -2,55 +2,51 @@
   <v-container>
     <v-row>
       <v-col cols="6">
-        <label for="task-title" class="form-label">Task Title</label>
+        <label for="task-title" class="form-label">{{ $t('components.newTaskForm.taskTitleLabel') }}</label>
       </v-col>
       <v-col cols="2">
-        <label for="task-priority" class="form-label">Priority</label>
+        <label for="task-priority" class="form-label">{{ $t('components.newTaskForm.priorityLabel') }}</label>
       </v-col>
       <v-col cols="2">
-        <label for="due-date" class="form-label">Due Date</label>
+        <label for="due-date" class="form-label">{{ $t('components.newTaskForm.dueDateLabel') }}</label>
       </v-col>
       <v-col cols="2"></v-col>
     </v-row>
     <v-row align="stretch" class="new-task-form">
       <v-col cols="6">
-        <v-text-field id="task-title" v-model="title" placeholder="Task title" outlined dense></v-text-field>
+        <v-text-field id="task-title" v-model="title" :placeholder="$t('components.newTaskForm.taskTitlePlaceholder')" outlined dense></v-text-field>
       </v-col>
       <v-col cols="2">
-        <v-select id="task-priority" v-model="priority" :items="['Low', 'Medium', 'High']" outlined dense></v-select>
+        <v-select id="task-priority" v-model="priority" :items="priorityOptions" outlined dense></v-select>
       </v-col>
       <v-col cols="2">
-        <v-text-field id="due-date" v-model="dueDate" placeholder="Select due date" type="date" outlined dense></v-text-field>
+        <v-text-field id="due-date" v-model="dueDate" :placeholder="$t('components.newTaskForm.dueDatePlaceholder')" type="date" outlined dense></v-text-field>
       </v-col>
       <v-col cols="2">
-        <v-btn color="primary" block class="h-100" @click="addTask" aria-label="Add Task"> Add Task </v-btn>
+        <v-btn color="primary" block class="h-100" @click="addTask" aria-label="Add Task">{{ $t('components.newTaskForm.addTaskButton') }}</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<style scoped>
-.form-label {
-  font-weight: bold;
-  font-size: large;
-  margin-left: 10px;
-  display: block;
-}
-</style>
 
 <script>
 export default {
   data() {
     return {
       title: '',
-      priority: 'Medium',
-      dueDate: ''
+      dueDate: '',
+      priorityOptions: [
+        this.$t('components.newTaskForm.priorityOptions.low'),
+        this.$t('components.newTaskForm.priorityOptions.medium'),
+        this.$t('components.newTaskForm.priorityOptions.high')
+      ],
+      priority: this.$t('components.newTaskForm.priorityOptions.medium')
     };
   },
   methods: {
     async addTask() {
       if (!this.title.trim() || !this.dueDate) {
-        alert('Task title and due date are required.');
+        alert(this.$t('components.newTaskForm.errorMessage'));
         return;
       }
 
@@ -69,7 +65,7 @@ export default {
         });
 
         if (!response.ok) {
-          throw new Error('Failed to add task');
+          throw new Error(this.$t('components.newTaskForm.taskAddedFailure'));
         }
 
         const newTask = await response.json();
@@ -81,7 +77,7 @@ export default {
     },
     resetForm() {
       this.title = '';
-      this.priority = 'Medium';
+      this.priority = this.$t('components.newTaskForm.priorityOptions.medium');
       this.dueDate = '';
     }
   }
@@ -89,6 +85,12 @@ export default {
 </script>
 
 <style scoped>
+.form-label {
+  font-weight: bold;
+  font-size: large;
+  margin-left: 10px;
+  display: block;
+}
 .v-input__control,
 .v-btn {
   height: 56px !important;
