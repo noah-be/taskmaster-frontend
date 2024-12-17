9a50104c514b97f4628102c9acabd1319d0f4acd
noah-frank <nolefr@posteo.de>
2024-12-17 18:36:02 +0100

    Update LoginForm component to use localization

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,29 +3,41 @@
     <v-card class="pa-4" outlined>
       <v-card-text>
         <v-form @submit.prevent="submitLogin">
-          <label for="username" class="v-label">Username</label>
-          <v-text-field id="username" v-model="username" placeholder="Username" autocomplete="username" outlined
-            dense></v-text-field>
+          <label for="username" class="v-label">{{ $t('components.loginForm.usernameLabel') }}</label>
+          <v-text-field
+            id="username"
+            v-model="username"
+            :placeholder="$t('components.loginForm.usernamePlaceholder')"
+            autocomplete="username"
+            outlined
+            dense
+          ></v-text-field>
 
-          <label for="password" class="v-label">Password</label>
-          <v-text-field id="password" v-model="password" placeholder="Password" type="password"
-            autocomplete="current-password" outlined dense></v-text-field>
+          <label for="password" class="v-label">{{ $t('components.loginForm.passwordLabel') }}</label>
+          <v-text-field
+            id="password"
+            v-model="password"
+            :placeholder="$t('components.loginForm.passwordPlaceholder')"
+            type="password"
+            autocomplete="current-password"
+            outlined
+            dense
+          ></v-text-field>
 
-          <v-btn class="mt-4" color="primary" block type="submit"> Log In </v-btn>
+          <v-btn class="mt-4" color="primary" block type="submit">{{ $t('components.loginForm.loginButton') }}</v-btn>
         </v-form>
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-actions>
-        <v-btn color="secondary" block class="mt-2" @click.prevent="showregisterBox = true"> Create New Account </v-btn>
+        <v-btn color="secondary" block class="mt-2" @click.prevent="showregisterBox = true">{{ $t('components.loginForm.createAccountButton') }}</v-btn>
       </v-card-actions>
     </v-card>
 
     <RegisterBox :show="showregisterBox" @update:show="showregisterBox = $event" />
   </v-container>
 </template>
-
 
 <script>
 import { ref } from 'vue';
