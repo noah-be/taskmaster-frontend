3df96ff844f2372dd915216f3c24ae170c2b45b2
noah-frank <nolefr@posteo.de>
2024-12-17 17:41:20 +0100

    Update AboutView.vue to use localization

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,11 +4,7 @@
       <v-col>
         <v-card outlined>
           <v-card-text>
-            <p class="mb-4">
-              This page is a personal project that I use as part of my studies to explore and learn both backend and frontend technologies. Initially, the
-              project was built using plain HTML, CSS, and JavaScript, but I am now transitioning it into a full-stack application to meet the requirements of
-              my studies.
-            </p>
+            <p class="mb-4">{{ $t('views.about.personalProject') }}</p>
             <p class="mb-4">
               The backend is powered by <strong>Node.js</strong> and <strong>MongoDB</strong>, both of which are already implemented. I have also integrated
               <strong>JWT</strong> for secure user authentication. Moving forward, I will be enhancing the backend further with features like
@@ -20,10 +16,12 @@
               Feel free to explore the features and technologies I've implemented. If you have any questions or just want to say hello, feel free to
               <router-link to="/contact">contact me</router-link>.
             </p>
-            <p>Thank you for visiting and showing interest in my learning project!</p>
+            <p>{{ $t('views.about.thankYou') }}</p>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+// TODO: Figure out how to format translatoins without using html tags in translation text
