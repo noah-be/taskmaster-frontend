5b7c44fab924ca2c05535d748cced16aeb557117
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-05 14:22:04 +0100

    Add Storybook stories for RegistrationGuidelines component

---
+++ src/components/RegistrationGuidelines.stories.js
@@ -0,0 +1,27 @@
+import RegistrationGuidelines from './RegistrationGuidelines.vue';
+
+export default {
+  title: 'Components/RegistrationGuidelines',
+  component: RegistrationGuidelines,
+  argTypes: {
+    visible: { control: 'boolean' }
+  }
+};
+
+const Template = args => ({
+  components: { RegistrationGuidelines },
+  setup() {
+    return { args };
+  },
+  template: '<RegistrationGuidelines v-bind="args" />'
+});
+
+export const Visible = Template.bind({});
+Visible.args = {
+  visible: true
+};
+
+export const Hidden = Template.bind({});
+Hidden.args = {
+  visible: false
+};
