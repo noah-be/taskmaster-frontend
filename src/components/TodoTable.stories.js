65488b9f6427ea4002e9a25dfb480e97b4aa7d16
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-05 14:27:15 +0100

    Add Storybook stories for TodoTable component

---
+++ src/components/TodoTable.stories.js
@@ -0,0 +1,47 @@
+import TodoTable from './TodoTable.vue';
+
+export default {
+  title: 'Components/TodoTable',
+  component: TodoTable,
+  argTypes: {
+    tasks: { control: 'array' }
+  }
+};
+
+const Template = args => ({
+  components: { TodoTable },
+  setup() {
+    return { args };
+  },
+  template: '<TodoTable v-bind="args" />'
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  tasks: [
+    {
+      _id: '1',
+      title: 'Task 1',
+      description: 'Description of Task 1',
+      dueDate: '2025-01-15',
+      priority: 'High',
+      completed: false
+    },
+    {
+      _id: '2',
+      title: 'Task 2',
+      description: 'Description of Task 2',
+      dueDate: '2025-01-20',
+      priority: 'Medium',
+      completed: true
+    },
+    {
+      _id: '3',
+      title: 'Task 3',
+      description: 'Description of Task 3',
+      dueDate: '2025-01-25',
+      priority: 'Low',
+      completed: false
+    }
+  ]
+};
