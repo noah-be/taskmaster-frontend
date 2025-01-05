c5eb24f1edb0948f6a57a992f8922e6de2d652f4
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-05 09:27:29 +0100

    Add Storybook stories for Footer component

---
+++ src/components/Footer.stories.js
@@ -0,0 +1,12 @@
+import Footer from './Footer.vue';
+export default {
+  title: 'Components/Footer',
+  component: Footer
+};
+
+const Template = () => ({
+  components: { Footer },
+  template: '<Footer />'
+});
+
+export const Default = Template.bind({});
