7ba7ceb224fb477d3683e8a421b01ff9d166df5f
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-09 14:42:21 +0100

    Add @storybook/addon-a11y for improved accessibility support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
+  ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
