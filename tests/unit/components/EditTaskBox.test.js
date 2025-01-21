279e6b363cba6dbffd41516b9bb1c3944add6a3a
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-21 08:59:26 +0100

    Improve EditTaskBox render tests

--- tests/unit/components/EditTaskBox.test.js
+++ tests/unit/components/EditTaskBox.test.js
@@ -1,27 +1,34 @@
 import EditTaskBox from '@/components/EditTaskBox.vue';
+import { createTestingPinia } from '@pinia/testing';
+import { useTaskStore } from '@/stores/taskStore';
 
 describe('EditTaskBox', () => {
-  let wrapper;
+  let wrapper, taskStore;
 
   beforeEach(() => {
-    wrapper = mount(EditTaskBox);
+    wrapper = mount(EditTaskBox, {
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    });
+
+    taskStore = useTaskStore();
   });
 
   afterEach(() => {
     wrapper.unmount();
   });
 
-  it('should render the dialog', () => {
+  it('should not render the dialog if currentTaskId is not set', async () => {
+    const dialog = wrapper.findComponent({ name: 'VDialog' });
+    expect(dialog.props('modelValue')).toBe(false);
+  });
+
+  it('should render the dialog if currentTaskId is set', async () => {
+    taskStore.currentTaskId = 1;
+    await wrapper.vm.$nextTick();
     const dialog = wrapper.findComponent({ name: 'VDialog' });
     expect(dialog.exists()).toBe(true);
     expect(dialog.props('modelValue')).toBe(true);
-  });
-
-  it('should not render the dialog if currentTaskId is not set', async () => {
-    const mockPiniaStore = wrapper.vm.$pinia._s.get('task');
-    mockPiniaStore.currentTaskId = null;
-    await wrapper.vm.$nextTick();
-    const dialog = wrapper.findComponent({ name: 'VDialog' });
-    expect(dialog.props('modelValue')).toBe(false);
   });
 });
--- tests/vitest.setup.js
+++ tests/vitest.setup.js
@@ -1,33 +1,8 @@
 import { vi } from 'vitest';
 
 import { createVuetify } from 'vuetify';
-import { createPinia, setActivePinia } from 'pinia';
 import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import { mount } from '@vue/test-utils';
-import { defineStore } from 'pinia';
-
-function createPiniaMock() {
-  const pinia = createPinia();
-  setActivePinia(pinia);
-
-  defineStore('task', {
-    state: () => ({
-      tasks: [{ _id: '1', title: 'Task 1', description: 'Do something important', dueDate: '2023-01-01', priority: 'High' }],
-      currentTaskId: '1'
-    }),
-    actions: {
-      fetchTasks: vi.fn(),
-      toggleTaskCompletion: vi.fn(),
-      addTask: vi.fn(),
-      deleteTask: vi.fn(),
-      updateTask: vi.fn(),
-      openEditTaskBox: vi.fn(),
-      closeEditDialog: vi.fn()
-    }
-  })();
-
-  return pinia;
-}
 
 function createI18nMock() {
   return {
@@ -50,10 +25,9 @@ global.beforeEach = beforeEach;
 global.afterEach = afterEach;
 
 const mockVuetify = createVuetify();
-const mockPinia = createPiniaMock();
 const mockI18n = createI18nMock();
 
 global.mount = (component, options) =>
   mount(component, {
-    global: { plugins: [mockVuetify, mockPinia], config: { globalProperties: { ...mockI18n.global.config.globalProperties } }, ...options }
+    global: { plugins: [mockVuetify], config: { globalProperties: { ...mockI18n.global.config.globalProperties } }, ...options }
   });
