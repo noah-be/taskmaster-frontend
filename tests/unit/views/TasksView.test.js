16a826dd8291a0cb25943577d5c44bf7f0581382
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-24 09:08:27 +0100

    Add TasksView tests

--- tests/unit/views/TasksView.test.js
+++ tests/unit/views/TasksView.test.js
@@ -1,7 +1,25 @@
 import TasksView from '@/views/TasksView.vue';
+import { createTestingPinia } from '@pinia/testing';
+import { useTaskStore } from '@/stores/taskStore';
 
-describe('TasksView.vue', () => {
-  it('should render TodoTable and EditTaskBox', () => {
-    expect(true).toBe(true);
+describe('TasksView', () => {
+  let wrapper, taskStore;
+
+  beforeEach(() => {
+    wrapper = mount(TasksView, {
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    });
+
+    taskStore = useTaskStore();
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('calls fetchTasks on mounted', () => {
+    expect(taskStore.fetchTasks).toHaveBeenCalled();
   });
 });
