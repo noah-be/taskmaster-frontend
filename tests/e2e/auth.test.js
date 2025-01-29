3cbee2e73aa6ef12a4cf7bdaf6a525c9fa4b646e
Noah <10984564+noah-be@users.noreply.github.com>
2025-01-29 10:39:30 +0100

    Change fixture scope to worker

--- tests/e2e/auth.test.js
+++ tests/e2e/auth.test.js
@@ -1,17 +1,8 @@
 import { test, expect } from './fixtures/auth';
 
 test.describe.serial('User Authentication', () => {
-  test('Register a new user', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.click('button:has-text("Create new account")');
-
-    const username = `testuser${Date.now()}`;
-    await page.fill('#register-username', username);
-    await page.fill('#register-password', 'Password123#!');
-    await page.click('button:has-text("Sign up")');
-    await page.waitForURL('**/tasks', { waitUntil: 'load' });
-
-    expect(page.url()).toBe('http://localhost:3000/tasks');
+  test('Register new user', async ({ registeredUser }) => {
+    await expect(registeredUser.page.url()).toBe('http://localhost:3000/tasks');
   });
 
   test('Login with registered user', async ({ authPage }) => {
--- tests/e2e/fixtures/auth.js
+++ tests/e2e/fixtures/auth.js
@@ -1,16 +1,45 @@
 import { test as base, expect } from '@playwright/test';
 
 export const test = base.extend({
-  authPage: async ({ page }, use) => {
-    await page.goto('http://localhost:3000');
-    await page.fill('#login-username', 'testuser');
-    await page.fill('#login-password', '^n#T%x$n^43&q7d3N6U7');
-    await page.click('button:has-text("Log in")');
-    await page.waitForURL('**/tasks', { waitUntil: 'load' });
+  authPage: [
+    async ({ browser }, use) => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
 
-    expect(page.url()).toBe('http://localhost:3000/tasks');
-    await use(page);
-  }
+      await page.goto('http://localhost:3000');
+      await page.fill('#login-username', 'testuser');
+      await page.fill('#login-password', '^n#T%x$n^43&q7d3N6U7');
+      await page.click('button:has-text("Log in")');
+      await page.waitForURL('**/tasks', { waitUntil: 'load' });
+
+      expect(page.url()).toBe('http://localhost:3000/tasks');
+      await use(page);
+
+      await context.close();
+    },
+    { scope: 'worker' }
+  ],
+
+  registeredUser: [
+    async ({ browser }, use) => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
+
+      await page.goto('http://localhost:3000');
+      await page.click('button:has-text("Create new account")');
+
+      const username = `testuser${Date.now()}`;
+      await page.fill('#register-username', username);
+      await page.fill('#register-password', 'Password123#!');
+      await page.click('button:has-text("Sign up")');
+      await page.waitForURL('**/tasks', { waitUntil: 'load' });
+
+      await use({ page, username });
+
+      await context.close();
+    },
+    { scope: 'worker' }
+  ]
 });
 
 export { expect };
